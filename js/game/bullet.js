45aef786df22a68bf2ec3f064545cee56693fefc
G014A1356\KoheiNagano <knagano0209@gmail.com>
2017-05-29 02:25:00 +0900

    攻撃の画像追加に伴い、コードの変更 画像の透過処理はまだできてません。

--- js/game/bullet.js
+++ js/game/bullet.js
@@ -4,11 +4,7 @@
 // 
 // ============================================================================
 
-// - const --------------------------------------------------------------------
-var SHOT_SPEED = 2.0;
-
 // - bullet ----------------------------------------------------------------
-
 var Bullet = function(id, ctx, position, mouse) {
     this.screenSize = new Point(position.x * 2, position.y * 2);
     this.BULLET_COLOR = 'rgba(0, 0, 255, 0.75)';
@@ -20,22 +16,26 @@ var Bullet = function(id, ctx, position, mouse) {
     this.size = 0;
     this.speed = 0;
     this.deadLength = this.screenSize.x;
+    this.img = new Image();
 
     if(id === "shine"){
         this.BULLET_COLOR = 'rgba(125, 255, 125, 0.75)';
         this.size = 10;
         this.speed = 5;
+        this.img.src = "image/game/punch.png";
     }
     else if(id === "shadow"){
         this.BULLET_COLOR = 'rgba(0, 0, 0, 0.75)';
         this.size = 20;
         this.speed = 10;
+        this.img.src = "image/game/shadow_ball.png";
     }
     else if(id === "comet"){
         this.BULLET_COLOR = 'rgba(255, 255, 125, 0.75)';
         this.size = 30;
         this.speed = 15;
-        this.deadLength = 10;
+        this.deadLength = 50;
+        this.img.src = "image/game/punch.png";
     }
 }
 
@@ -44,19 +44,18 @@ Bullet.prototype.Update = function () {
     this.position.y += this.velocity.y * this.speed;
 
     var length = new Point(this.position.x - this.screenSize.x / 2, this.position.y - this.screenSize.y / 2);
-    length.Length();
-    if (length > this.deadLength){
+    if (length.Length() > this.deadLength){
         this.isDead = true;
     }
 }
 
 Bullet.prototype.Draw = function () {
-    drawCircle(this.ctx, this.position, this.size, this.BULLET_COLOR);
+    // 描画
+	this.ctx.drawImage(this.img, this.position.x - this.img.width / 2, this.position.y - this.img.height / 2);
 }
 
 Bullet.prototype.IsCollide = function(position, radius){
-    //return this.position.Distance(position) < this.img.width /2 + radius;
-    return this.position.Distance(position) < this.size /2 + radius;
+    return this.position.Distance(position) < this.size / 2 + radius;
 }
 
 Bullet.prototype.Collide = function () {
